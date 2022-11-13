43db29e1fcfd5703e59cc1f74bf6fbf291be0884
unknown <nnv2205owo@gmail.com>
2022-11-13 20:16:36 +0700

    lfs

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def video_feed(id):
     return Response(obj.detect(yolo_opt[int(id)], db),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# flask_app.run(host='127.0.0.1', debug=True)
+flask_app.run(host='127.0.0.1', debug=True)
 
 # import pafy
 #
